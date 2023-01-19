f6108ec8ee6a6c7c7e8b9c81cf77a8f80ba4229d
Keffmen <bylka500@gmail.com>
2023-01-20 04:49:03 +0700

    V 0.0.2.1
    Сделал взрвы при уничтожении нло

--- Assets/Script/Alien attack/left_ufo_movement.cs
+++ Assets/Script/Alien attack/left_ufo_movement.cs
@@ -6,10 +6,13 @@ public class left_ufo_movement : MonoBehaviour
 {
     public float timeEnd = 0;
     public float speed = 0.1f;
+    public GameObject Destroy_effec;
 
     private void OnMouseDown()
     {
+        GameObject expo = Instantiate(Destroy_effec, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        Destroy(expo,1);
         Score.alien += 1;
     }
 
